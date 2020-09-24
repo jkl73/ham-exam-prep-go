c090aae1b729b9608cfe33cf9d37ebe4d6c52185
Jiankun Lu <jl3387@cornell.edu>
2020-09-24 01:26:19 -0700

    clear

--- client/src/Ham-Question-Pool.js
+++ client/src/Ham-Question-Pool.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Card, Header, Form, Input, Icon } from "semantic-ui-react";
+import { Card, Header, Form, Input, Icon, Button } from "semantic-ui-react";
 
 
 let endpoint = "http://192.168.0.82:8080";
 
-let qpb = require('./ham-questions-pool_pb')
+let qpb = require('./ham-questions-pool_pb');
+
+let palegreen = "rgb(190,251,152)";
+let bluuu = "rgba(135,200,230,12)";
+let reddd = "rgb(240,128,128)";
 
 class HamQuestion extends Component {
 
@@ -15,44 +19,66 @@ class HamQuestion extends Component {
         this.state = {
             stem: "",
             choices: [],
-            cardsColor: ["grey","grey","grey","grey"],
+            key: -1,
+            cardsColor: ["transparent","transparent","transparent","transparent"],
+            answerSelected: -1,
         };
     }
 
     componentDidMount() {
-        console.log("componentDidMount")
         this.getQuestion()
     }
 
     cardClicked = (which) => {
+        let colrs = ["transparent","transparent","transparent","transparent"];
+        colrs[which] = bluuu
+        
         this.setState({
-            cardsColor: ["green","grey","grey","grey"],
+            cardsColor: colrs,
+            answerSelected: which
         });
     };
 
+    showAnswer = () => {
+        let colrs = ["transparent","transparent","transparent","transparent"];
+        colrs[this.state.key] = palegreen
+
+        this.setState({
+            cardsColor: colrs,
+        });
+    }
+
+    checkAnswer = () => {
+        let colrs = this.state.cardsColor
+
+
+        if (this.state.answerSelected == this.state.key) {
+            colrs[this.state.key] = palegreen
+        } else {
+            colrs[this.state.answerSelected ] = reddd
+            colrs[this.state.key] = palegreen
+        }
+
+
+        this.setState({
+            cardsColor: colrs,
+        });
+    }
+
     getQuestion = () => {
         axios.get(endpoint + "/api/newq", {responseType:'arraybuffer'}).then(res => {
-  
-
             if (res.data) {
-
                 var qqq = qpb.Question.deserializeBinary(res.data)
-                console.log(qqq)
-
-                
-                console.log(qqq.getDistractorsList())
                 var distractorsl = qqq.getDistractorsList()
-                console.log(qqq.getStem())
-                console.log(qqq.getKey())
 
-                // select random
+                // select random as key
                 let keyloc = Math.floor(Math.random()*4)
                 distractorsl.splice(keyloc,0, qqq.getKey())
-                console.log(keyloc)
 
                 this.setState({
                     stem: qqq.getStem(),
                     choices: distractorsl,
+                    key: keyloc
                 });
             } else {
                 this.setState({
@@ -67,7 +93,7 @@ class HamQuestion extends Component {
           <div>
             <div className="row">
               <Header className="header" as="h2">
-                HAM General Exam Questions Pool
+                HAM General Exam Practice 📻
               </Header>
             </div>
             <div className="row">
@@ -75,9 +101,9 @@ class HamQuestion extends Component {
                     <Card color="yellow" fluid>
                         <Card.Content>
                             <Card.Header textAlign="center">
-                                <div style={{ wordWrap: "break-word" }}>{this.state.stem}</div>
+                                <div style={{ fontSize: "20px", wordWrap: "break-word" }}>{this.state.stem}</div>
                             </Card.Header>
-                            <Card color={this.state.cardsColor[0]} fluid onClick={() => this.cardClicked(0)}>
+                            <Card fluid onClick={() => this.cardClicked(0)}>
                                 <Card.Content style={{ background: this.state.cardsColor[0] }}>
                                     <Card.Header textAlign="center">
                                         <div style={{ wordWrap: "break-word" }}>{this.state.choices[0]}</div>
@@ -93,8 +119,8 @@ class HamQuestion extends Component {
                                 </Card.Content>
                             </Card>
 
-                            <Card color="grey" fluid>
-                                <Card.Content>
+                            <Card fluid onClick={() => this.cardClicked(1)}>
+                                <Card.Content style={{ background: this.state.cardsColor[1] }}>
                                     <Card.Header textAlign="center">
                                         <div style={{ wordWrap: "break-word" }}>{this.state.choices[1]}</div>
                                     </Card.Header>
@@ -109,8 +135,8 @@ class HamQuestion extends Component {
                                 </Card.Content>
                             </Card>
 
-                            <Card color="grey" fluid>
-                                <Card.Content>
+                            <Card fluid onClick={() => this.cardClicked(2)}>
+                            <Card.Content style={{ background: this.state.cardsColor[2] }}>
                                     <Card.Header textAlign="center">
                                         <div style={{ wordWrap: "break-word" }}>{this.state.choices[2]}</div>
                                     </Card.Header>
@@ -125,8 +151,8 @@ class HamQuestion extends Component {
                                 </Card.Content>
                             </Card>
 
-                            <Card color="grey" fluid>
-                                <Card.Content>
+                            <Card fluid onClick={() => this.cardClicked(3)}>
+                            <Card.Content style={{ background: this.state.cardsColor[3] }}>
                                     <Card.Header textAlign="center">
                                         <div style={{ wordWrap: "break-word" }}>{this.state.choices[3]}</div>
                                     </Card.Header>
@@ -141,15 +167,22 @@ class HamQuestion extends Component {
                                 </Card.Content>
                             </Card>
 
-
-                            <Card.Meta textAlign="right">     
+                            <Card.Meta textAlign="right">
+                            <div className="ui buttons">
+                                <div className="ui red basic button" onClick={() => this.showAnswer()}>Don't Know</div>
+                                <div className="ui green basic button" onClick={() => this.checkAnswer()}>Confirm Answer</div>
+                                <div className="ui blue basic button">Next Question</div>
+                            </div>
+                            </Card.Meta>
+                            
+                            {/* <Card.Meta textAlign="right">     
                                 <Icon
                                   name="delete"
                                   color="red"
                                   // onClick={() => this.deleteTask(item._id)}
                                 />
                                 <span style={{ paddingRight: 10 }}>Delete</span>
-                            </Card.Meta>
+                            </Card.Meta> */}
                         </Card.Content>    
                     </Card>
                 </Card.Group>
--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -17,12 +17,12 @@ func GetQuestion(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	payload := proto.Question{
-		Sublement:   "IMSUBLE",
-		Sequence:    2,
-		Chapter:     "CH3",
-		Stem:        "STEMM",
-		Key:         "KEYY",
-		Distractors: []string{"DIS1", "DIS2", "DIS3"},
+		Sublement:   "G1B",
+		Sequence:    3,
+		Chapter:     "[97.3(a)(9)]",
+		Stem:        "Which of the following is a purpose of a beacon station as identified in the FCC rules?",
+		Key:         "Observation of propagation and reception",
+		Distractors: []string{"Automatic identification of repeaters", "Transmission of bulletins of general interest to Amateur Radio licensees", "Identifying net frequencies"},
 		Figure:      "",
 	}
 
