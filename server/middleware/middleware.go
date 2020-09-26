a915921dfb7665e206814d75f6abcf7fceb1bcf1
Jiankun Lu <jl3387@cornell.edu>
2020-09-26 03:04:42 -0700

    server can return an exam set 35 questions

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 
 	pb "google.golang.org/protobuf/proto"
-	// "encoding/json"
 
 	"net/http"
 
@@ -25,29 +24,57 @@ func initQ() *hamquestions.HamQuestion {
 	return q
 }
 
-// GetQuestion get one question
-func GetQuestion(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/octet-stream'") // send
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	payload := getOneRandQuestion()
-	msg, err := pb.Marshal(payload)
-
+// GetSimExam returns a complete exam questions set
+func GetSimExam(w http.ResponseWriter, r *http.Request) {
+	exampb := getSimExamQuestions()
+	msg, err := pb.Marshal(exampb)
 	if err != nil {
 		fmt.Println(err)
 	}
+	sendGeneric(w, r, msg)
+}
 
-	bytesSent, err := w.Write(msg)
+// GetQuestion get one question
+func GetQuestion(w http.ResponseWriter, r *http.Request) {
+	question := getOneRandQuestion()
+	msg, err := pb.Marshal(question)
 	if err != nil {
-		fmt.Println("failed to send")
+		fmt.Println(err)
+	}
+	sendGeneric(w, r, msg)
+}
+
+func sendGeneric(w http.ResponseWriter, r *http.Request, payload []byte) {
+	w.Header().Set("Context-Type", "application/octet-stream'") // send
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	bytesSent, err := w.Write(payload)
+	if err != nil {
+		fmt.Println("Failed to send")
 		fmt.Println(err)
 	} else {
 		fmt.Printf("Sent %d\n", bytesSent)
 	}
 }
 
+func getSimExamQuestions() *proto.QuestionList {
+	res := proto.QuestionList{}
+	for _, sub := range q.Pool.Subl {
+		for _, qList := range sub.GetGroupMap() {
+			nq := rand.Intn(len(qList.GetQuestions()))
+			res.Questions = append(res.Questions, qList.GetQuestions()[nq])
+		}
+	}
+	return &res
+}
+
 func getOneRandQuestion() *proto.Question {
 	nsbl := rand.Intn(len(q.Pool.Subl))
-	nq := rand.Intn(len(q.Pool.Subl[nsbl].Qlist))
-	return q.Pool.Subl[nsbl].Qlist[nq]
+	// get the first one
+	for _, qList := range q.Pool.Subl[nsbl].GetGroupMap() {
+		nq := rand.Intn(len(qList.GetQuestions()))
+
+		return qList.GetQuestions()[nq]
+	}
+	return nil
 }
--- server/router/router.go
+++ server/router/router.go
@@ -9,5 +9,6 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/newq", middleware.GetQuestion).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/exam", middleware.GetSimExam).Methods("GET", "OPTIONS")
 	return router
 }
